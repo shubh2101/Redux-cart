f08f538886d3ac32ebb420f829dc47e45f381f7f
Shubham Singh <shubham2101@gmail.com>
2022-08-14 21:00:14 +0530

    Cart funtionality using reduxjs/toolkit

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,17 @@
 import classes from './CartButton.module.css';
+import { useSelector,useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
+ const cartQuantity = useSelector(state => state.cart.totalQuantity)
+const dispatch = useDispatch()
+const toggleHandler = () => {
+  dispatch(uiActions.toggle())
+}
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,21 +1,34 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
-
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+const Dummy_Products = [
+  {
+    id: "p1",
+    title: "Football",
+    price: 6,
+    description: "This is a first product - amazing!",
+  },
+  {
+    id: "p2",
+    title: "Book",
+    price: 3,
+    description: "This is a second product - amazing!",
+  },
+];
 const Products = (props) => {
+  
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Football'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-        <ProductItem
-          title='Book'
-          price={3}
-          description='This is a second product - amazing!'
-        />
+        {Dummy_Products.map((product) => (
+          <ProductItem
+            key={product.id}
+            id = {product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
